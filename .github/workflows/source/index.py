28212b9f16340b3c960d8507581d41c51e5907c0
figeral <mouliofitzgerard@gmail.com>
2024-04-01 10:45:11 +0100

    lunching telegram actions

--- .github/workflows/source/index.py
+++ .github/workflows/source/index.py
@@ -2,9 +2,9 @@ import requests
 
 
 def send_mgs():
-    token = "bot${{secrets.TELEGRAM_BOT_TOKEN}}"
+    token = "bot6544277286:AAHSPQdHgM8YIKVQM0fGJzXXN04iudE3pNg"
     url = f"https://api.telegram.org/{token}/sendMessage"
-    user_id = "${{secrets.FITZ_ID}}"
+    user_id = 5429946779
     msg = "A ${{github.event.type}} made by ${{github.event.author}}  ,  fille free to edit it at ${{github.repository.html_url}}"
     payload = dict(chat_id=user_id, text=msg)
     requests.post(url=url, params=payload)
