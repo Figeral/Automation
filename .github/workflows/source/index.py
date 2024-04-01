f3cd5c740d8281c1165fddbe779217c960c31449
figeral <mouliofitzgerard@gmail.com>
2024-04-01 19:31:18 +0100

    modifications done on actions

--- .github/workflows/source/index.py
+++ .github/workflows/source/index.py
@@ -4,7 +4,7 @@ import os
 
 def send_mgs():
     token = os.environ['TELEGRAM_BOT_TOKEN']
-    user_id = os.environ['USER_ID']
+    user_id = 2123403309
     repository = os.environ['GITHUB_REPOSITORY']
     author = os.environ['GITHUB_EVENT_AUTHOR']
 
