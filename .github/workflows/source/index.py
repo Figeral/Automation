95ab1b7c99993eee2756959708d3c3ee3b916e6c
figeral <mouliofitzgerard@gmail.com>
2024-04-01 20:25:03 +0100

    modifications done on actions

--- .github/workflows/source/index.py
+++ .github/workflows/source/index.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 
 
 def send_mgs():
@@ -7,12 +8,12 @@ def send_mgs():
     user_id = os.environ['USER_ID']
     repository = os.environ['GITHUB_REPOSITORY']
     author = os.environ['GITHUB_EVENT_AUTHOR']
-
+    date = time.ctime()
     baseUrl = "https://api.telegram.org"
     url = f"{baseUrl}/bot{token}/sendMessage"
     link = f"github.com/{repository}"
 
-    msg = f"A Pushed 🚀 made by {author} \n feel free to edit it at {link}"
+    msg = f"A Pushed 🚀 made by {author}✨ made on {date} \n \n feel free to edit it at {link}"
     payload = dict(chat_id=user_id, text=msg)
     requests.post(url=url, params=payload)
 
