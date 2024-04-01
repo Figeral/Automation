cc9c4dec4ea2a548a2e4ef0b4da9b4594685e105
figeral <mouliofitzgerard@gmail.com>
2024-04-01 10:31:56 +0100

    telegram action done

--- .github/workflows/source/index.py
+++ .github/workflows/source/index.py
@@ -1,1 +1,14 @@
-print("hello  world")
+import requests
+
+
+def send_mgs():
+    token = "bot${{secrets.TELEGRAM_BOT_TOKEN}}"
+    url = f"https://api.telegram.org/{token}/sendMessage"
+    user_id = "${{secrets.FITZ_ID}}"
+    msg = "A ${{github.event.type}} made by ${{github.event.author}}  ,  fille free to edit it at ${{github.repository.html_url}}"
+    payload = dict(chat_id=user_id, text=msg)
+    requests.post(url=url, params=payload)
+
+
+if __name__ == "__main__":
+    send_mgs
